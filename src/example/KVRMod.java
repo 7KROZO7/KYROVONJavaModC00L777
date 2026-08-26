bb2b15eba70f73ca1482e04c9c0824cd449538c3
7KROZO7 <nanaytiu@gmail.com>
2026-08-26 18:36:17 +0800

    Update and rename ExampleJavaMod.java to KVRMod.java😎

---
+++ src/example/KVRMod.java
@@ -0,0 +1,20 @@
+package extra;
+
+import mindustry.mod.Mod;
+
+public class KVRMod extends Mod {
+
+    public KVRMod() {
+        // Constructor
+    }
+
+    @Override
+    public void loadContent() {
+        // Content loading logic goes here
+    }
+
+    @Override
+    public void init() {
+        // Initialization logic goes here
+    }
+}
