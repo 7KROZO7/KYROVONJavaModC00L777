a43f14941aac563f9f88659986056af52f15b28b
7KROZO7 <nanaytiu@gmail.com>
2026-08-28 05:58:13 +0800

    Update KVRUnits.java

--- src/content/KVRUnits.java
+++ src/content/KVRUnits.java
@@ -10,7 +10,7 @@ public class KVRUnits {
 
     public static void load() {
         ping = new UnitType("ping") {{
-            localizedName = "Ping"; // Correct Java field name
+            localizedName = "Ping";
             description = "A friendly dimensional observer drone.";
             controller = u -> new CompanionAI();
             constructor = UnitEntity::create;
