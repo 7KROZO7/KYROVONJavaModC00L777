f68a795eba986b0d93441917b8e568876e5350eb
7KROZO7 <nanaytiu@gmail.com>
2026-08-27 21:48:47 +0800

    Update KVRUnits.java

--- src/content/KVRUnits.java
+++ src/content/KVRUnits.java
@@ -1,14 +1,19 @@
 package extra.content;
 
 import arc.graphics.Color;
+import arc.struct.Seq;
 import extra.ai.CompanionAI;
+import mindustry.ai.types.MinerAI;
+import mindustry.content.Items;
 import mindustry.gen.UnitEntity;
 import mindustry.type.UnitType;
 
 public class KVRUnits {
     public static UnitType ping;
+    public static UnitType riftMite;
 
     public static void load() {
+        // --- PING ---
         ping = new UnitType("ping") {{
             localizedName = "Ping";
             description = "A friendly dimensional observer drone.";
@@ -37,6 +42,43 @@ public class KVRUnits {
             trailLength = 12;
             trailColor = Color.valueOf("a855f7");
             outlineColor = Color.valueOf("2a1645");
+        }};
+
+        // --- RIFT MITE ---
+        riftMite = new UnitType("rift-mite") {{
+            localizedName = "Rift Mite";
+            description = "A short-lived interdimensional mining drone.";
+            controller = u -> new MinerAI();
+            constructor = UnitEntity::create;
+
+            flying = true;
+            drag = 0.06f;
+            accel = 0.15f;
+            speed = 2.6f;
+            hitSize = 6f;
+            health = 150f;
+            itemCapacity = 20;
+
+            // Mining settings
+            mineTier = 3;
+            mineSpeed = 4.5f;
+            mineRange = 70f;
+            mineFloor = true;
+            mineWalls = true;
+
+            mineItems = Seq.with(
+                Items.copper, Items.lead, Items.coal, Items.titanium,
+                Items.scrap, Items.sand, Items.beryllium, Items.tungsten
+            );
+
+            targetable = false;
+            hittable = false;
+            isEnemy = false;
+            playerControllable = false;
+
+            engineSize = 1.4f;
+            engineColor = Color.valueOf("38bdf8");
+            outlineColor = Color.valueOf("1e132e");
         }};
     }
 }
