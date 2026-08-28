35aa2c0a0a9717dfb8140e6f23f52b76ea175fbe
7KROZO7 <nanaytiu@gmail.com>
2026-08-28 17:49:53 +0800

    Update KVRUnits.java

--- src/content/KVRUnits.java
+++ src/content/KVRUnits.java
@@ -2,13 +2,16 @@ package extra.content;
 
 import arc.graphics.Color;
 import extra.ai.CompanionAI;
+import extra.ai.MiteAI;
 import mindustry.gen.UnitEntity;
 import mindustry.type.UnitType;
 
 public class KVRUnits {
     public static UnitType ping;
+    public static UnitType riftMite;
 
     public static void load() {
+        // --- PING ---
         ping = new UnitType("ping") {{
             localizedName = "Ping";
             description = "A friendly dimensional observer drone.";
@@ -37,6 +40,34 @@ public class KVRUnits {
             trailLength = 12;
             trailColor = Color.valueOf("a855f7");
             outlineColor = Color.valueOf("2a1645");
+        }};
+
+        // --- RIFT MITE ---
+        riftMite = new UnitType("rift-mite") {{
+            localizedName = "Rift Mite";
+            description = "A short-lived interdimensional mining drone.";
+            controller = u -> new MiteAI(); // Custom lag-free AI
+            constructor = UnitEntity::create;
+
+            flying = true;
+            hovering = true;
+            shadowElevation = 0.15f;
+            speed = 2.6f;
+            accel = 0.15f;
+            drag = 0.06f;
+            hitSize = 6f;
+            health = 150f;
+            itemCapacity = 20;
+
+            mineTier = 2;
+            mineSpeed = 4.5f;
+            mineRange = 70f;
+            mineFloor = true;
+            mineWalls = true;
+
+            engineSize = 1.4f;
+            engineColor = Color.valueOf("38bdf8");
+            outlineColor = Color.valueOf("1e132e");
         }};
     }
 }
