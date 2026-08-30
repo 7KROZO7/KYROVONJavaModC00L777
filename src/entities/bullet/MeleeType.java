f3c3c12c41360ac7277da5b4c10aad80eecaed37
7KROZO7 <nanaytiu@gmail.com>
2026-08-30 09:43:04 +0800

    Update MeleeType.java

--- src/entities/bullet/MeleeType.java
+++ src/entities/bullet/MeleeType.java
@@ -2,6 +2,7 @@ package extra.entities.bullet;
 
 import arc.graphics.Color;
 import extra.content.KVREffects;
+import mindustry.Vars;
 import mindustry.content.Fx;
 import mindustry.content.StatusEffects;
 import mindustry.entities.bullet.BulletType;
@@ -22,18 +23,22 @@ public class MeleeType extends BulletType {
         this.collidesAir = false;
         this.pierce = false;
 
+        // Hit FX & Audio (Plays when teeth crunch an enemy)
         this.hitEffect = KVREffects.voidBite;
+        this.hitSound = Vars.tree.loadSound("bite");
+        this.hitSoundVolume = 1.2f;
+        this.hitColor = Color.valueOf("ef4444");
+
         this.despawnEffect = Fx.none;
         this.shootEffect = Fx.none;
         this.smokeEffect = Fx.none;
-        this.hitColor = Color.valueOf("ef4444"); // Red
 
         this.status = StatusEffects.slow;
         this.statusDuration = 45f;
     }
 
     public MeleeType(float damage) {
-        this(damage, 36f);
+        this(damage, 38f);
     }
 
     @Override
